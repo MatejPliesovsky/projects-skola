bc17cb2d542c248df64da7b588ad0d41d31415db
MatejPliesovsky <matej.pliesovsky@akademiasovy.sk>
2016-03-31 09:06:37 +0200

    new files

--- Quiz/Quiz/Form1.Designer.cs
+++ Quiz/Quiz/Form1.Designer.cs
@@ -43,7 +43,6 @@
             this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.radioButton5 = new System.Windows.Forms.RadioButton();
             this.radioButton6 = new System.Windows.Forms.RadioButton();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
@@ -53,6 +52,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -198,10 +198,10 @@
             this.radioButton5.AutoSize = true;
             this.radioButton5.Location = new System.Drawing.Point(150, 13);
             this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(84, 17);
+            this.radioButton5.Size = new System.Drawing.Size(65, 17);
             this.radioButton5.TabIndex = 13;
             this.radioButton5.TabStop = true;
-            this.radioButton5.Text = "Transportnej";
+            this.radioButton5.Text = "Linkovej";
             this.radioButton5.UseVisualStyleBackColor = true;
             // 
             // radioButton6
@@ -214,15 +214,6 @@
             this.radioButton6.TabStop = true;
             this.radioButton6.Text = "Sieťovej";
             this.radioButton6.UseVisualStyleBackColor = true;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(303, 352);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(136, 20);
-            this.dateTimePicker1.TabIndex = 15;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // checkBox1
             // 
@@ -323,17 +314,25 @@
             this.label8.Size = new System.Drawing.Size(0, 37);
             this.label8.TabIndex = 24;
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(268, 352);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 25;
+            this.dateTimePicker1.Value = new System.DateTime(2016, 3, 31, 0, 0, 0, 0);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(504, 514);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -374,7 +373,6 @@
         private System.Windows.Forms.RadioButton radioButton4;
         private System.Windows.Forms.RadioButton radioButton5;
         private System.Windows.Forms.RadioButton radioButton6;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox3;
@@ -384,6 +382,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- Quiz/Quiz/Form1.cs
+++ Quiz/Quiz/Form1.cs
@@ -18,173 +18,74 @@ namespace Quiz
            label8.Visible= false;
         }
          
-        int count = 0;
+        public int count = 0;
+
+        private System.DateTime rightDate = new System.DateTime(1970, 1, 1, 0, 0, 0);
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        private string Answer1 = "arpanet";
-        private string user1;
         
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
-            string date = dateTimePicker1.Value.ToShortDateString();
-            if (date == "1.1.1970")
+           System.DateTime date = new System.DateTime();
+            date = dateTimePicker1.Value;
+            if (date.Equals(rightDate))
             {
                 count++;
                 
             }
-            else
-            {
-                count = 0;
-            }
-
+           
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
           
-            if (radioButton1.Checked == true)
-            {
-                 if (radioButton1.Checked == true)
-                {
-                    count=0;
-                }
-                else
-                {
-                    count++;
-                }
-            }
-            else 
             if (radioButton2.Checked == true)
             {
-                if (radioButton1.Checked == true)
-                {
-                    count++;
+                count++;
                 }
-                else
-                {
-                    count=0;
-                }
-            }
-            else if (radioButton3.Checked == false)
-            {
-                if (radioButton1.Checked == true)
-                {
-                    count = 0;
-                }
-                else
-                {
-                    count++;
-                }
-            }
         }
 
         private void groupBox2_Enter(object sender, EventArgs e)
         {
-            
-            if (radioButton1.Checked == true)
+
+            if (radioButton5.Checked == true)
             {
-                if (radioButton1.Checked == true)
-                {
-                    count = 0;
-                }
-                else
-                {
-                    count++;
-                }
-            }
-            else if (radioButton2.Checked == true)
-            {
-                if (radioButton1.Checked == true)
-                {
-                    count = 0;
-                }
-                else
-                {
-                    count++;
-                }
-            }
-            else if (radioButton3.Checked == true)
-            {
-                if (radioButton1.Checked == true)
-                {
-                    count++;
-                }
-                else
-                {
-                    count=0;
-                }
+                count++;
             }
         }
 
         private void groupBox3_Enter(object sender, EventArgs e)
         {
-            if (checkBox1.Checked == true)
+            if (checkBox1.Checked == false && checkBox4.Checked == false && checkBox2.Checked == true && checkBox3.Checked == true)
             {
-                if (checkBox1.Checked == true)
-                {
-                    count++;
-                }
-                else
-                {
-                    count=0;
-                }
+                count++;
             }
-            else if (checkBox2.Checked == true)
-            {
-                if (checkBox2.Checked == true)
-                {
-                    count = 0;
-                }
-                else
-                {
-                    count=0;
-                }
-            }
-            else
-            if (checkBox3.Checked == true)
-            {
-                if (checkBox3.Checked == true)
-                {
-                    count++;
-                }
-                else
-                {
-                    count = 0;
-                }
-            }
-            else
-            if (checkBox4.Checked == true)
-            {
-                
-                    count=0;
-                }
-                else
-                {
-                    count=0;
-                }
-           
         }
 
         private void button1_Click(object sender, System.EventArgs e)
         {
-                        
-        label8.Visible = true;
+            count = 0;
+            groupBox1_Enter(button1, null);
+            groupBox2_Enter(button1, null);
+            groupBox3_Enter(button1, null);
+            comboBox1_SelectedIndexChanged(button1, null);
+            textBox1_TextChanged(button1, null);
+            dateTimePicker1_ValueChanged(button1, null);
+
+            label8.Visible = true;
             
             label8.Text = "Correct answers: " + count;
         }
         
         private void comboBox1_SelectedIndexChanged(object sender, CancelEventArgs e)
         {
-            if (comboBox1.SelectedItem.ToString().Equals("Hviezdicova"))
+            if (comboBox1.Text.Equals("Hviezdicova"))
             {
                 count++;
-            }
-            else
-            {
-                count = 0;
             }
         }
 
@@ -196,10 +97,6 @@ namespace Quiz
                 count++;
             }
 
-            else
-            {
-                count=0;
-            }
         }
     }
 
